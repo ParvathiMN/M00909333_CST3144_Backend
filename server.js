8a30ea8aa92218099cc233966d89ab3344936530
Parvathi Manoj <parvathi.m2004@gmail.com>
2024-11-30 10:42:58 +0400

    Added static image find code

--- server.js
+++ server.js
@@ -1,85 +1,81 @@
+// Importing required modules
 const express = require('express');
 const { result } = require('lodash');
 const MongoClient = require('mongodb').MongoClient;
 var path = require("path"); 
 var fs = require("fs"); 
 
-
 const app = express();
 
+// Middleware to parse JSON request bodies
 app.use(express.json());
 app.set('port', 3000);
-app.use((req,res,next) => {
+
+// Middleware for enabling Cross-Origin Resource Sharing (CORS)
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
     next();
-})
-
+});
 
 let db;
 
-MongoClient.connect('mongodb+srv://parvathim2004:1234@cluster0.acnmseg.mongodb.net/', (err, client)=>{
+// Connecting to MongoDB Atlas
+MongoClient.connect('mongodb+srv://parvathim2004:1234@cluster0.acnmseg.mongodb.net/', (err, client) => {
     db = client.db('CST3144_M00909333');
 });
 
-app.get('/',(req,res,next)=>{
+// Root route to show a basic response
+app.get('/', (req, res, next) => {
     res.send('Select a collection, e.g., /collection/messages');
 });
 
+// Middleware to identify collection names in the route
 app.param('collectionName', (req, res, next, collectionName) => { 
-     req.collection = db.collection(collectionName) 
-     return next() 
- });
+    req.collection = db.collection(collectionName); 
+    return next(); 
+});
 
-
- app.get('/collection/:collectionName', (req, res, next) => { 
+// Retrieve all documents from a specified collection
+app.get('/collection/:collectionName', (req, res, next) => { 
     req.collection.find({}).toArray((e, results) => { 
-    if (e) return next(e) 
-        res.send(results) 
-        }) 
-    })  
+        if (e) return next(e); 
+        res.send(results); 
+    }); 
+});
 
-   
+// Add a new document to a specified collection
+app.post('/collection/:collectionName', (req, res, next) => { 
+    req.collection.insert(req.body, (e, results) => { 
+        if (e) return next(e); 
+        res.send(results.ops); // Return the inserted document
+    }); 
+});
 
-    app.post('/collection/:collectionName', (req, res, next) => { 
-        req.collection.insert(req.body, (e, results) => { 
-        if (e) return next(e) 
-        res.send(results.ops)
-         }) 
-        }) 
+// Serve static files from the "image" directory
+app.use(function(req, res, next) { 
+    const filePath = path.join(__dirname, "image", req.url); 
+    fs.stat(filePath, function(err, fileInfo) { 
+        if (err) { 
+            next(); 
+            return; 
+        } 
+        if (fileInfo.isFile()) res.sendFile(filePath); 
+        else next(); 
+    }); 
+});
 
-        app.get('/collection/Orders', (req, res, next) => {
-            req.collection.find({}).toArray((error, results) => {
-                if (error) return next(error);
-                res.json(results); 
-            });
-        });
-        
-    const ObjectID = require('mongodb').ObjectID; 
-    app.get('/collection/:collectionName/:id', (req, res, next) => { 
-    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => { 
-    if (e) return next(e) 
-    res.send(result) 
-    }) 
-    }) 
+// Handle 404 errors for undefined routes
+app.use(function(req, res) { 
+    res.status(404); // Set status to 404
+    res.send("File not found!"); 
+});
 
-app.put('/collection/:collectionName/:id', (req, res, next) => { 
-    req.collection.update( 
-    {_id: new ObjectID(req.params.id)}, 
-    {$set: req.body}, 
-    {safe: true, multi: false},  
-    (e, result) => { 
-    if (e) return next(e) 
-    res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'}) 
-    }) 
-    })
-
-
-const port =process.env.PORT ||3000
-app.listen(port)
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port);
 
 // app.listen(3000,()=>{
 //     console.log('server.js server running at localhost:3000')
